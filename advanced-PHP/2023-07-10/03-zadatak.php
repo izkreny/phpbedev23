2d75a8f9ec713ba93b38d1aa04e321c537bdc8dd
izkreny <iskren.nemet@gmail.com>
2023-07-19 21:43:49 +0200

    Final solution! :-)

--- advanced-PHP/2023-07-10/03-zadatak.php
+++ advanced-PHP/2023-07-10/03-zadatak.php
@@ -1,15 +1,15 @@
 <?php
 
-$brojTel = "+012-345-Alo-Alo";
+$brojTel = "+012-345-6-Alo-Alo";
 
 try
 {
     // https://www.php.net/manual/en/function.preg-replace.php
     // https://www.php.net/manual/en/regexp.reference.escape.php
-    $filterBroj = preg_replace('/\D/', '', $brojTel);
+    $filtriraniBroj = preg_replace('/\D/', '', $brojTel);
 
-    if (strlen($filterBroj) >= 6) {
-        echo "Broj je ispravan: " . $filterBroj . "\n";
+    if (strlen($filtriraniBroj) <= 6) {
+        echo "Broj je ispravan: " . $filtriraniBroj . "\n";
     } else {
         throw new Exception ("Nešto nije u redu s brojem telefona!\n");
     }
