1fa7ae6bfd2fcbc1a0eaed318cf46cb0f82bf581
izkreny <iskren.nemet@gmail.com>
2023-08-25 18:44:15 +0200

    Prva datoteka.

---
+++ advanced-PHP/2023-08-25/KorisnikModel.php
@@ -0,0 +1,25 @@
+<?php
+
+    include_once 'Database.php';
+
+    class KorisnikModel
+    {
+        private $conn;
+        private $table = 'korisnici';
+
+        public function __construct($db)
+        {
+            $this->conn = $db;
+        }
+
+        public function emailPostoji($email)
+        {
+            // metoda mora vratiti ili istinu ili laž
+
+            $querry = "SELECT * FROM ". $this->table;
+            $stmt = $this->conn->prepare($querry);
+            $email = $stmt->execute()->fetchAll(PDO::FETCH_ASSOC);
+
+            return if_array($email, $emails, strict);
+        }
+    }
