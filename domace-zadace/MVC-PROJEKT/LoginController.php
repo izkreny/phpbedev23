2241335080748736b4423ad4412546127b24cf19
izkreny <iskren.nemet@gmail.com>
2023-08-29 19:26:24 +0200

    Login

---
+++ domace-zadace/MVC-PROJEKT/LoginController.php
@@ -0,0 +1,64 @@
+<?php
+
+    include_once 'KorisnikModel.php';
+    include_once 'Login.php';
+
+    class LoginController
+    {
+        private $model;
+        private $view;
+        private $poruke = [];
+
+        public function __construct()
+        {
+            $database = new Database('korisnici_db');
+            $this->model = new KorisnikModel($database->connect());
+            $this->view = new Login();
+        }
+
+        public function prikaziPoruke()
+        {
+            if (!empty($this->poruke)) {
+                $this->view->prikaziPoruke($this->poruke);
+            }
+        }
+
+        public function prikaziLogin()
+        {
+            $this->view->prikaziFormu();
+        }
+
+        private function provjeriEmail($email)
+        {
+            if ($this->model->postojiLi($email, 'email')) {
+                return true;
+            } else {
+                $this->poruke[] = "Email NE postoji u sustavu!";
+
+                return false;
+            }
+        }
+
+        public function obradiLogin($podaci)
+        {
+            if ($this->provjeriEmail($podaci['email'])) { // TODO: Provjeriti i je li korisnik AKTIVIRAN!!!
+                // https://www.php.net/manual/en/function.password-hash.php
+                $podaci['lozinka'] = password_hash($podaci['lozinka'], PASSWORD_DEFAULT);
+
+                var_dump($this->model->dohvatiLozinku($podaci['email'])); 
+                if (password_verify($podaci['lozinka'], $this->model->dohvatiLozinku($podaci['email']))) {
+                    $this->poruke[] = "Korisnik je uspješno prijavljen u sustav!";
+                } else {
+                    $this->poruke[] = "Korisnik NIJE uspješno prijavljen u sustav!";
+                }
+            }
+        }
+    }
+
+    $controller = new LoginController();
+    
+    if (!empty($_POST)) {
+        $controller->obradiLogin($_POST);
+    }
+    $controller->prikaziPoruke();
+    $controller->prikaziLogin();
